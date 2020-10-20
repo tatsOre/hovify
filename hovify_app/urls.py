a9d62afdb0db1401de3517d2f1b65760fd13bbf9
daorejuela1 <daorejuela1@outlook.com>
2020-10-20 04:29:49 +0000

    added prefix api/v1 to every api routes

--- hovify_app/urls.py
+++ hovify_app/urls.py
@@ -9,25 +9,25 @@ from rest_framework.authtoken import views
 
 router = ExtendedSimpleRouter()
 (
-    router.register(r'users', UserViewSet, basename='user').register(r'projects', ProjectViewSet, basename='users-project', parents_query_lookups=['userID']),
-    router.register(r'users', UserViewSet, basename='user').register(r'professionals', ProfessionalViewSet, basename='user-professional', parents_query_lookups=['userID']),
-    router.register(r'users', UserViewSet, basename='user').register(r'educations', EducationViewSet, basename='user-education', parents_query_lookups=['userID']),
-    router.register(r'users', UserViewSet, basename='user').register(r'languages', LanguagesViewSet, basename='user-languages', parents_query_lookups=['userID']),
-    router.register(r'users', UserViewSet, basename='user').register(r'techskills', TechSkillViewSet, basename='user-techskill', parents_query_lookups=['userID']),
-    router.register(r'users', UserViewSet, basename='user').register(r'interests', InterestViewSet, basename='user-interests', parents_query_lookups=['userID']),
-    router.register(r'users', UserViewSet, basename='user').register(r'aboutusers', AboutUserViewSet, basename='user-aboutusers', parents_query_lookups=['userID']),
-    router.register(r'users', UserViewSet, basename='user').register(r'motivations', MotivationViewSet, basename='user-motivations', parents_query_lookups=['userID']),
-    router.register(r'users', UserViewSet, basename='user').register(r'desiredjobfields', DesiredJobFieldViewSet, basename='user-desiredjobfields', parents_query_lookups=['userID']),
-    router.register(r'users', UserViewSet, basename='user').register(r'desiredjoblocations', DesiredJobLocationViewSet, basename='user-desiredjoblocations', parents_query_lookups=['userID']),
-    router.register(r'users', UserViewSet, basename='user').register(r'vacancies', VacancyUserViewSet, basename='vacancy', parents_query_lookups=['users']),
-    router.register(r'vacancies', VacancyViewSet, basename='vacancy'),
+    router.register(r'api/v1/users', UserViewSet, basename='user').register(r'projects', ProjectViewSet, basename='users-project', parents_query_lookups=['userID']),
+    router.register(r'api/v1/users', UserViewSet, basename='user').register(r'professionals', ProfessionalViewSet, basename='user-professional', parents_query_lookups=['userID']),
+    router.register(r'api/v1/users', UserViewSet, basename='user').register(r'educations', EducationViewSet, basename='user-education', parents_query_lookups=['userID']),
+    router.register(r'api/v1/users', UserViewSet, basename='user').register(r'languages', LanguagesViewSet, basename='user-languages', parents_query_lookups=['userID']),
+    router.register(r'api/v1/users', UserViewSet, basename='user').register(r'techskills', TechSkillViewSet, basename='user-techskill', parents_query_lookups=['userID']),
+    router.register(r'api/v1/users', UserViewSet, basename='user').register(r'interests', InterestViewSet, basename='user-interests', parents_query_lookups=['userID']),
+    router.register(r'api/v1/users', UserViewSet, basename='user').register(r'aboutusers', AboutUserViewSet, basename='user-aboutusers', parents_query_lookups=['userID']),
+    router.register(r'api/v1/users', UserViewSet, basename='user').register(r'motivations', MotivationViewSet, basename='user-motivations', parents_query_lookups=['userID']),
+    router.register(r'api/v1/users', UserViewSet, basename='user').register(r'desiredjobfields', DesiredJobFieldViewSet, basename='user-desiredjobfields', parents_query_lookups=['userID']),
+    router.register(r'api/v1/users', UserViewSet, basename='user').register(r'desiredjoblocations', DesiredJobLocationViewSet, basename='user-desiredjoblocations', parents_query_lookups=['userID']),
+    router.register(r'api/v1/users', UserViewSet, basename='user').register(r'vacancies', VacancyUserViewSet, basename='vacancy', parents_query_lookups=['users']),
+    router.register(r'api/v1/vacancies', VacancyViewSet, basename='vacancy'),
 )
 
 urlpatterns = [
-        path('signup/', UserCreate.as_view(), name='createuser'),
-        path('curriculums/<int:pk>/', CurriculumDetail.as_view(), name='professional_view'),
-        path('curriculum/', LoggedCurriculum.as_view(), name='getcurriculum'),
-        path("login/", views.obtain_auth_token, name="login"),
+        path('api/v1/signup/', UserCreate.as_view(), name='createuser'),
+        path('api/v1/curriculums/<int:pk>/', CurriculumDetail.as_view(), name='professional_view'),
+        path('api/v1/curriculum/', LoggedCurriculum.as_view(), name='getcurriculum'),
+        path("api/v1/login/", views.obtain_auth_token, name="login"),
 ]
 
 urlpatterns += router.urls
