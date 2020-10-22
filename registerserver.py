8ea6b1216f82d057705cbe62643c8d0f47dd3c69
daorejuela1 <daorejuela1@outlook.com>
2020-10-22 02:01:32 +0000

    added print to know the form fields

--- registerserver.py
+++ registerserver.py
@@ -19,16 +19,16 @@ and the user-agent to use.
 
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
-browser = mechanicalsoup.StatefulBrowser(user_agent)
+browser = mechanicalsoup.StatefulBrowser()
 browser.open("https://www.linkedin.com/checkpoint/lg/login?trk=hb_signin")
 browser.select_form('form[action="/checkpoint/lg/login-submit"]')
 browser["session_key"] = environ.get('USER_LINKEDIN')
-browser["session_password"] = environ.get('PASS_LINKEDIN')d
+browser["session_password"] = environ.get('PASS_LINKEDIN')
 response = browser.submit_selected()
 if response.status_code != 200:
     raise ValueError("Response status is not 200, check credentials")
 browser.select_form()
-browser.get_current_form().print_summary()
+print(browser.get_current_form().print_summary())
 
 val = input("Enter your value: ")
 print(val)
