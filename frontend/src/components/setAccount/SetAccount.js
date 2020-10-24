a37c83561f1b981fc5fbff0c3c6ac90f6517940c
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-10-24 21:05:46 +0000

    Stored data was submit Stored data from the user was submit and move to the CV Builder

--- frontend/src/components/setAccount/SetAccount.js
+++ frontend/src/components/setAccount/SetAccount.js
@@ -4,7 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import { Link, useHistory } from 'react-router-dom';
 import './SetAccount.css';
 import { context } from '../../App.js';
-import { getUser, createAccount } from '../../api/ApiRequest.js';
+import { createAccount, getLogin, getUser } from '../../api/ApiRequest.js';
 
 export default function SetAccount() {
   /* ------------------------------------------------------ */
@@ -16,20 +16,53 @@ export default function SetAccount() {
       mode: "onBlur"
   });
   // Submit stored data from the user and move to the CV Builder:
-  const onLogin = data => {
-    console.log(data);
-    history.push('/builder');
-    // createAccount(JSON.stringify(data))
-    // .then(response => {
-    //   console.log(response)
-    // })
+  const onCreate = data => {
+    const newAccount = {
+      "email": data.email,
+      "password": data.password
+    };
+    const userAccount = {
+      "username": data.email,
+      "password": data.password,
+    }
+    console.log(newAccount);
+    //history.push('/builder');
+    createAccount(JSON.stringify(newAccount))
+      .then(response => {
+        const promiseData = response.json()
+        promiseData.then(data => {
+		  console.log(data);
+          getLogin(JSON.stringify(userAccount))
+          .then(response => {
+            const promiseLogin = response.json()
+            promiseLogin.then(dataLogin => {
+              const userToken = "Token " + dataLogin.token
+              context.token = userToken;
+              console.log("Context: " + context.token);
+              getUser(context.token)
+              .then(response => {
+			    const promiseUser = response.json()
+				promiseUser.then(dataUser => {
+                  const userInfo = dataUser
+				  console.log(userInfo);
+				})
+			  })
+            })
+            console.log(response.status)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+		})
+        //console.log(response.json())
+      })
   };
-  
+
   return (
   <section className="account">
     <aside className='branding'>HoviFy</aside>
     <h1 className="account-title"><span>{userData.User.FirstName},<br/></span>we need the last details to create an account:</h1>
-    <form onSubmit={handleSubmit(onLogin)} className="form-account">
+    <form onSubmit={handleSubmit(onCreate)} className="form-account">
       <div className="label-content">
         <label className="acount-label" for="UserName">Your e-mail:</label>
         <label className="acount-label" for="password">Password:</label>
@@ -40,19 +73,19 @@ export default function SetAccount() {
           id="UserName"
           className="textField-account"
           type="email"
-          name="Email"
-          inputRef={register({required: true, maxLength: 80})} />  
+          name="email"
+          inputRef={register({required: true, maxLength: 80})} />
         <TextField
           id="password"
           className="textField-account"
           type="password"
-          name="Password"
+          name="password"
           inputRef={register({required: true, maxLength: 80})} />
         <TextField
           id="confirm-password"
           className="textField-account"
           type="password"
-          name="Password"
+          name="checkpass"
           inputRef={register({required: true, maxLength: 80})} />
       </div>
       <nav className="nav-account">
@@ -64,7 +97,3 @@ export default function SetAccount() {
   </section>
   );
 }
-
-
-
-
