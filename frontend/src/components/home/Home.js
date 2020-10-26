032fd182a2a45fc88c36b8fef859ffab88bb6642
tatsOre <tatianaorezap@gmail.com>
2020-10-26 16:13:59 -0500

    update Home

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -13,10 +13,11 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import LockIcon from '@material-ui/icons/Lock';
 import CloseIcon from '@material-ui/icons/Close';
 import { context } from '../../App.js';
+import Fade from 'react-reveal/Fade';
 
 import './Home.css';
-import './logo_2.png';
-
+import graphic from './img/main-graphic.svg';
+import people from './img/people.jpg';
 /* API */
 import { getLogin, getUser, postUser, createAccount, getProfile } from '../../api/ApiRequest.js';
 
@@ -25,71 +26,6 @@ export default function Home() {
   /* Login implementation */
   let userToken = "Token "
   let userData = ""
-
-  
-  
-  /*const dataUser = {
-    "User": {
-        "id": 5,
-        "user": {
-            "email": "juanllano93@gmail.com"
-        },
-        "FirstName": "Juan Nathaly",
-        "LastName": "Llano",
-        "Location": "Colombia",
-        "City": "Tulua",
-        "PhoneNumber": "123456789",
-        "Birthday": null,
-        "Summary": "My Summary",
-        "LinkedIn": null,
-        "PortfolioURL": null,
-        "GitHubURL": null,
-        "TwitterURL": null
-    },
-    "Education": [],
-    "Professional": [],
-    "Skills": [],
-    "Languages": [],
-    "Projects": [],
-    "About_User": [],
-    "Motivation": [],
-    "Interest": [],
-    "Desired_Job_Fields": [],
-    "Desired_Job_Location": [],
-    "Vacancy": []
-  }
-
-  const dataAccount = {
-    "email": "juanito1@gmail.com",
-    "password": "123456"
-  }
-
-  
-
-  React.useEffect(() => {
-    getLogin(JSON.stringify(login))
-    .then(response => {
-      userToken += String(response.token)
-      console.log(userToken)
-      /*------ Fetch to curriculum with header  authorization ------/
-      getUser(userToken)
-      .then(response => {
-            userData = response
-            console.log(userData)
-            postUser(JSON.stringify(dataUser), userToken)
-          })
-      /*-----------------------*
-    })
-    .catch()
-  }, []);
-
-  /*React.useEffect(() => {
-    createAccount(JSON.stringify(dataAccount))
-    .then(response => console.log(response))
-  }, []);*/
-
-  
-
   /* ------------------------------------------------------ */
   const history = useHistory();
   /* ------------------------------------------------------ */
@@ -137,10 +73,7 @@ export default function Home() {
     .catch( error => {
       console.log(error)
     })
-
   }
-
-
   const {
     register: register2,
     errors: errors2,
@@ -159,24 +92,13 @@ export default function Home() {
       const promiseProfile = response.json();
       promiseProfile.then(data => {
         const userProfile = data;
-        //console.log("token")
-        //console.log(userProfile);
-        
         context.user = userProfile;
         hiddenLinkedin();
         history.push('/hello');
-
-      //  console.log("Context" );
-    //    console.log( context.user);
       })
       console.log(response.status)
     })
-    
-    
   };
-  // console.log("home");
-  // console.log(context.user);
-
   /* Sign in Modal: */
   const loginForm = (
     <div className="modal-signin">
@@ -244,7 +166,8 @@ export default function Home() {
     <div className="home">
       <StylesProvider injectFirst>
         <header className="home-header">
-          <div className="logo"><img src="./logo_2.png" alt="Hovify Logo"/></div>
+          <people></people>
+          <div className="logo"></div>
           <div className="signin-model__container">
             <p className="signin-text">Already a Hovifier?</p>
             <button className="btn-signin" onClick={showLogin}>Sign in</button >
@@ -259,25 +182,26 @@ export default function Home() {
           </div>
         </header>
         <main className="main-content">
-          <div className="imgHome">
-            <img
-              src="https://st2.depositphotos.com/5779744/8395/v/450/depositphotos_83959630-stock-illustration-teamwork-concept-of-group-of.jpg"
-              alt="Person"
-            />
-          </div>
           <section className="contentHome">
-            <h1>We are awesome.</h1>
-              <p>
+            <h1 className="main-title"><span>We</span><span> are</span> awesome.</h1>
+            <Fade right>
+              <p className="p1">
                 In Hovify we believe that anybody with the right attitude should have a real chance
-                of applying for a job. And all starts with a good looking resume. </p>
+                of applying for a job. </p>
+                <p>And all starts with a good looking resume.</p>
+            </Fade>
               <p>
-                That's how Hovify comes in handy, saving you time building your resume the best way possible
+                That's how HoviFy comes in handy, saving you time building your resume the best way possible
                 increasing your chances to call the attention of recruiters. </p>
               <p>
-                But wait! there's more.
-                Hovify can also help you match with job offers that fit with your professional profile and preferences.</p>
-
-            <h3>We believe that you are awesome. Let us know about you and we will show you some Hovify magic: </h3>
+                But wait! there's more:<br/>
+                HoviFy can also help you match with job offers that fit with your professional profile and preferences.</p>
+                <div>
+        <Fade right>
+        <h3>We believe that you are awesome. Let us know about you and we will show you some Hovify magic: </h3>
+        </Fade>
+      </div>
+            
 
             <div id="start-nav">
               <button className="btn-primary_linkedin" type="button" onClick={showLinkedin}>Fill-up with your LinkedIn profile</button >
@@ -295,12 +219,31 @@ export default function Home() {
           </section>
         </main>
         <section className="hovify-team">
-          :)
-          AndrÃ©s Bayona.
-          David Orejuela.
-          Juan Llano.
-          Nathaly Sotomayor.
-          Tatiana Orejuela.
+          <div className="hovify-member">
+            <div className="hovify-member__photo"></div>
+            <p className="hovify-member__name">Andres Bayona</p>
+            <p className="hovify-member__label">Aquarius</p>
+          </div>
+          <div className="hovify-member">
+            <div className="hovify-member__photo"></div>
+            <p className="hovify-member__name">David Orejuela</p>
+            <p className="hovify-member__label">Scorpio</p>
+          </div>
+          <div className="hovify-member">
+            <div className="hovify-member__photo"></div>
+            <p className="hovify-member__name">Juan Sebastián Llano</p>
+            <p className="hovify-member__label">Scorpio</p>
+          </div>
+          <div className="hovify-member">
+            <div className="hovify-member__photo"></div>
+            <p className="hovify-member__name">Nathaly Sotomayor</p>
+            <p className="hovify-member__label">Cancer</p>
+          </div>
+          <div className="hovify-member">
+            <div className="hovify-member__photo"></div>
+            <p className="hovify-member__name">Tatiana Orejuela</p>
+            <p className="hovify-member__label">Pisces</p>
+          </div>
         </section>
         <footer className="foonavbar">
           <Link className="foonavbar-link" to="/">Home</Link>
